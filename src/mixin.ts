5721c241e8cfc09c543ffab8fc730ec001b33190
Snazzah <suggesttosnazzy@gmail.com>
2022-04-05 23:22:22 -0500

    chore: add workflows and linting

--- src/mixin.ts
+++ src/mixin.ts
@@ -1,10 +1,12 @@
 export default {
-  created (this: any) {
+  created(this: any) {
     if (this.$ssrContext && this.$page.frontmatter.ld) {
       const ld = this.$page.frontmatter.ld;
       let script = '';
-      if (Array.isArray(ld)) for (const ldi of ld) script += `\n    <script type="application/ld+json">${JSON.stringify({'@context': 'https://schema.org', ...ldi})}</script>`;
-      else script += `\n    <script type="application/ld+json">${JSON.stringify({'@context': 'https://schema.org', ...ld})}</script>`
+      if (Array.isArray(ld))
+        for (const ldi of ld)
+          script += `\n    <script type="application/ld+json">${JSON.stringify({ '@context': 'https://schema.org', ...ldi })}</script>`;
+      else script += `\n    <script type="application/ld+json">${JSON.stringify({ '@context': 'https://schema.org', ...ld })}</script>`;
       this.$ssrContext.pageMeta += script;
     }
   },
@@ -14,14 +16,14 @@ export default {
   },
 
   methods: {
-    refreshJSONLD (this: any) {
+    refreshJSONLD(this: any) {
       Array.from(document.querySelectorAll('head script[type="application/ld+json"]')).forEach((el) => el.remove());
       if (this.$page.frontmatter.ld) {
         const ld = Array.isArray(this.$page.frontmatter.ld) ? this.$page.frontmatter.ld : [this.$page.frontmatter.ld];
         for (const ldi of ld) {
           const script = document.createElement('script');
           script.setAttribute('type', 'application/ld+json');
-          script.innerHTML = JSON.stringify({'@context': 'https://schema.org', ...ldi});
+          script.innerHTML = JSON.stringify({ '@context': 'https://schema.org', ...ldi });
           document.head.appendChild(script);
         }
       }
@@ -29,12 +31,12 @@ export default {
   },
 
   watch: {
-    $page (this: any) {
+    $page(this: any) {
       this.refreshJSONLD();
     }
   },
 
-  beforeDestroy () {
+  beforeDestroy() {
     Array.from(document.querySelectorAll('head script[type="application/ld+json"]')).forEach((el) => el.remove());
   }
-}
+};
